4c9513e4c57b653a1facfb8e3aabb9aeb8f7f00f
ngbaduyy10 <ngbaduyy05@gmail.com>
2024-10-07 09:59:29 +0700

    homepage created

---
+++ src/store/cartSlice/index.jsx
@@ -0,0 +1,77 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import {getCart, addToCart, updateCart, deleteCart} from '@/services/cart.service.jsx';
+
+export const getCartSlice = createAsyncThunk(
+    "cart/getCart",
+    async (id) => {
+        return await getCart(id);
+    }
+);
+
+export const addToCartSlice = createAsyncThunk(
+    "cart/addToCart",
+    async (data) => {
+        return await addToCart(data);
+    }
+);
+
+export const updateCartSlice = createAsyncThunk(
+    "cart/updateCart",
+    async (data) => {
+        return await updateCart(data);
+    }
+);
+
+export const deleteCartSlice = createAsyncThunk(
+    "cart/deleteCart",
+    async (data) => {
+        return await deleteCart(data);
+    }
+);
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        cartItems: [],
+        loading: false,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getCartSlice.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(getCartSlice.fulfilled, (state, action) => {
+                state.cartItems = action.payload.data.items;
+                state.loading = false;
+            })
+            .addCase(getCartSlice.rejected, (state) => {
+                state.loading = false;
+            })
+            .addCase(addToCartSlice.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(addToCartSlice.fulfilled, (state, action) => {
+                state.cartItems = action.payload.data.items;
+                state.loading = false;
+            })
+            .addCase(addToCartSlice.rejected, (state) => {
+                state.loading = false;
+            })
+            .addCase(updateCartSlice.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(updateCartSlice.fulfilled, (state, action) => {
+                state.cartItems = action.payload.data.items;
+                state.loading = false;
+            })
+            .addCase(updateCartSlice.rejected, (state) => {
+                state.loading = false;
+            })
+            .addCase(deleteCartSlice.fulfilled, (state, action) => {
+                state.cartItems = action.payload.data.items;
+            })
+    }
+});
+
+export default cartSlice.reducer;
